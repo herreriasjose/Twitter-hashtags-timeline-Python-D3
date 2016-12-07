45f2a5d5f73a3364bec71ad7f683cbcb8b356af4
José Herrerías <herreriasjose@gmail.com>
2016-12-07 12:04:35 +0100

    Update twitter_api.py

--- twitter_api.py
+++ twitter_api.py
@@ -1,5 +1,7 @@
-API_KEY =	"uszRU7CBLx5MhDx1lYpaFUxZM" # Consumer Key (API Key)
-API_SECRET =	"dl8bzTdMfExRehPw9XhoSf2bDzix4xBQhK1zh3HuDV1QOVepwq" # Consumer Secret (API Secret)
+# You must change this keys with yours
 
-TOKEN_KEY =		"2329449824-Vju5G1VzL2SzWQwEcToAIaeP1Mt3gu3ZyLiL2Xb" # Access Token
-TOKEN_SECRET =	"irHMEBpYonWoyNH8qktF7ymPUlSz1mCNTE4qB9GtNVX7c" # Access Token Secret
+API_KEY =	"##########################" # Consumer Key (API Key)
+API_SECRET =	"##########################" # Consumer Secret (API Secret)
+
+TOKEN_KEY =		"##########################" # Access Token
+TOKEN_SECRET =	"##########################" # Access Token Secret
